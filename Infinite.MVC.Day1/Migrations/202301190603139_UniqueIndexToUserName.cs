36ad8aed0c711abef78bf7c4db13b3916c6559c9
Chandrashekar Jogiyappa <chandrashekarj@infinite.com>
2023-01-19 16:04:40 +0530

    Add project files.

---
+++ Infinite.MVC.Day1/Migrations/202301190603139_UniqueIndexToUserName.cs
@@ -0,0 +1,17 @@
+﻿namespace Infinite.MVC.Day1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UniqueIndexToUserName : DbMigration
+    {
+        public override void Up()
+        {
+            CreateIndex("dbo.Users", "Username", true);
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
